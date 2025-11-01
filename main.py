9153adf3515c1bf6df013eea364565093132cff4
Denotess <tian.istenic34@gmail.com>
2025-11-01 20:38:03 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,187 @@
+import http.server, os
+
+class case_no_file(object):
+    def test(self, handler):
+        return not os.path.exists(handler.full_path)
+    
+    def act(self, handler):
+        raise Exception("'{}' not found".format(handler.path))
+    
+class case_existing_file(object):
+    def test(self, handler):
+        return os.path.isfile(handler.full_path)
+
+    def act(self, handler):
+        handler.handle_file(handler.full_path)
+
+class case_always_fail(object):
+    def test(self, handler):
+        return True
+
+    def act(self, handler):
+        raise Exception("Unknown object '{0}'".format(handler.path))
+
+class case_directory_index_file(object):
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        return  os.path.isdir(handler.full_path, 'index.html') and \
+                os.path.isfile(self.index_path(handler))
+
+    def act(self, handler):
+        handler.handle_file(self.index_path(handler))
+
+class case_directory_no_index_file(object):
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+    
+    def test(self, handler):
+        return  os.path.isdir(handler.full_path) and \
+                not os.path.isfile(self.index_path(handler))
+    
+    def act(self, handler):
+        handler.list_dir(handler.full_path)
+
+class case_cgi_file(object):
+    def test(self, handler):
+        return  os.path.isfile(handler.full_path) and \
+                handler.full_path.endswith('.py')
+    def act(self, handler):
+        handler.run_cgi(handler.full_path)
+
+    def run_cgi(self, full_path):
+        cmd = "python " + full_path
+        child_stdin, child_stdout = os.popen2(cmd)
+        child_stdin.close()
+        data = child_stdout.read()
+        child_stdout.close()
+        self.send_content(data)
+
+
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    Cases = [case_no_file,
+             case_cgi_file,
+             case_existing_file,
+             case_directory_index_file,
+             case_directory_no_index_file,
+             case_always_fail
+             ]
+    Error_Page = """\
+        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+        """
+    Page = '''\
+        <html>
+        <body>
+        <table>
+        <tr>  <td>Header</td>         <td>Value</td>          </tr>
+        <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
+        <tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
+        <tr>  <td>Client port</td>    <td>{client_port}s</td> </tr>
+        <tr>  <td>Command</td>        <td>{command}</td>      </tr>
+        <tr>  <td>Path</td>           <td>{path}</td>         </tr>
+        </table>
+        </body>
+        </html>
+        '''
+    def do_GET(self):
+        try:
+            #full_path = os.getcwd() + self.path
+            self.full_path = os.path.join(os.getcwd(), self.path.lstrip('/'))
+
+            for case in self.Cases:
+                handler = case()
+                if handler.test(self):
+                    handler.act(self)
+                    break
+
+            """if not os.path.exists(full_path):
+                raise Exception("'{0}' not found".format(self.path))
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+                return
+            else:
+                raise Exception("Unknow object '{0}'".format(self.path))"""
+        except Exception as msg:
+            self.handle_error(msg)
+            return
+    
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+    Error_Page = """\
+        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+        """
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content, 404)
+    
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        if isinstance(content, str):
+            self.wfile.write(content.encode('utf-8'))
+        else:
+            self.wfile.write(content)
+
+    def create_page(self):
+        values = {
+            'date_time': self.date_time_string(),
+            'client_host': self.client_address[0],
+            'client_port' : self.client_address[1],
+            'command' : self.command,
+            'path' : self.path
+        }
+
+        page = self.Page.format(**values)
+        return page
+
+    def send_page(self, page):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(self.Page)))
+        self.end_headers()
+        self.wfile.write(page.encode('utf-8'))
+
+    Listing_Page = '''\
+    <html>
+    <body>
+    <ul>
+    {0}
+    </ul>
+    </body>
+    </html>
+    '''
+
+    def list_dir(self, full_path):
+        try: 
+            entries = os.listdir(full_path)
+            bullets = ['<li>{0}</li>'.format(e) 
+                       for e in entries if not e.startsiwth('.')]
+            page = self.Listing_Page.format('\n'.join(bullets))
+            self.send_content(page)
+        except OSError as msg:
+            msg = "'{0}' cannot be listed: {1}".format(self.path, msg)
+            self.handle_error(msg)
+
+if __name__ == '__main__':
+    serverAddress = ('', 8080)
+    server = http.server.HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()
